25c1b3905bd9677f82fffd4b24ab3cff5f083aab
fleurvaneijk <37218048+fleurvaneijk@users.noreply.github.com>
2021-11-25 15:03:41 +0100

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  stripeKey: 'pk_test_3CwmIQMAWM1fm9j14DvmnKiY00uDKSMkDA',
+  firebase: {
+    apiKey: 'AIzaSyAivzfbkQMuzLr211u4eXSrCM0MBuaaeEw',
+    authDomain: 'hexagon-4d246.firebaseapp.com',
+    databaseURL: 'https://hexagon-4d246.firebaseio.com',
+    projectId: 'hexagon-4d246',
+    storageBucket: 'hexagon-4d246.appspot.com',
+    messagingSenderId: '52561084100',
+    appId: '1:52561084100:web:ced74f5c267c44f2'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  stripeKey: 'pk_test_3CwmIQMAWM1fm9j14DvmnKiY00uDKSMkDA',
+  firebase: {
+    apiKey: 'AIzaSyAivzfbkQMuzLr211u4eXSrCM0MBuaaeEw',
+    authDomain: 'hexagon-4d246.firebaseapp.com',
+    databaseURL: 'https://hexagon-4d246.firebaseio.com',
+    projectId: 'hexagon-4d246',
+    storageBucket: 'hexagon-4d246.appspot.com',
+    messagingSenderId: '52561084100',
+    appId: '1:52561084100:web:ced74f5c267c44f2'
+  }
+};
+
+export const firebaseConfig = {};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
